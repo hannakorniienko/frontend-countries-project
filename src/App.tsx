e8f8a6fee44d7946c1240adeb9136c5958a54ab1
unknown <altamar133@gmail.com>
2022-11-09 01:05:29 +0200

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
-import { useAppDispatch, useAppSelector } from './redux/hooks';
-import { fetchCountries, sortByName } from './redux/reducers/countries';
 import Home from './pages/Home';
 import SingleCountry from './pages/SingleCountry';
 
 const App = () => {
-  const countries = useAppSelector(state => state.countriesReducer)
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    dispatch(fetchCountries())
-  }, [])
   return (
-    <div className='App'>
+    <BrowserRouter>
       <Routes>
-        <BrowserRouter>
-          <Route path='/' element={<Home />}/>
-          <Route path=':name' element={<SingleCountry />} />
-        </BrowserRouter>
+      <Route path="/" element={<Home />}/>
+      <Route path=":id" element={<SingleCountry />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
       dispatch(fetchCountries())
     }, [])
     return (
-      <div className='App'>
+      <div>
         <button onClick={() => dispatch(sortByName())}>Sort countries</button>
         <table>
           <thead>
@@ -18,8 +18,8 @@ const Home = () => {
               <th>Name</th>
               <th>Capital</th>
               <th>Flag</th>
-              {/* <th>Currencies</th> */}
-              {/* <th>Languages</th> */}
+              <th>Currencies</th>
+              <th>Languages</th>
               <th>Region</th>
             </tr>
           </thead>
@@ -27,10 +27,10 @@ const Home = () => {
             {countries.map(item => (
               <tr>
                 <td key={item.name.official}>{item.name.official}</td>
-                <td>{item.capital}</td>
-                <td><img style={{ width: 100 }} src={item.flags.png} alt="flag" /></td>
-                {/* <td>{item.currencies}</td> */}
-                {/* <td>{item.languages}</td> */}
+                <td key={item.capital}>{item.capital}</td>
+                <td key={item.flags}><img style={{ width: 100 }} src={item.flags.png} alt="flag" /></td>
+                <td>{item.currencies}</td>
+                <td>{item.languages}</td>
                 <td>{item.region}</td>
               </tr>
             ))}

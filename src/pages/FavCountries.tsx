643e31e194b19578cf6c5d9284198a640fd91927
unknown <altamar133@gmail.com>
2022-11-26 23:51:40 +0200

    small changes in FavCountries page style

--- src/pages/FavCountries.tsx
+++ src/pages/FavCountries.tsx
@@ -6,13 +6,13 @@ import '../styles/favCountry.css'
 const FavCountries = () => {
     const {favCountriesList} = useAppSelector((state) => state.favCountries)
     return (
-        <div id='fav-country'>{favCountriesList.map(item => (
+        <div id='fav-country'>
+            <div>{favCountriesList.map(item => (
             <ul key={item.name.common}>
                 <li><img style={{ width: 20 }} src={item.flags.png} alt="flag" /> {item.name.common}</li>
-            </ul>
-        ))}
+            </ul>))}
+            </div>
         </div>)
-    
 }
 
 export default FavCountries

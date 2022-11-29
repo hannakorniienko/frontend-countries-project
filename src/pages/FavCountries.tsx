1ab87ba54e7dc8ae4be78eba0e73eca33fd4c57a
unknown <altamar133@gmail.com>
2022-11-29 23:00:48 +0200

    added delete button on FavCountries page

--- src/pages/FavCountries.tsx
+++ src/pages/FavCountries.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useAppSelector } from '../redux/hooks'
 
+import { useAppDispatch, useAppSelector } from '../redux/hooks'
+import { remove } from '../redux/reducers/favCountries'
 import '../styles/favCountry.css'
 
 const FavCountries = () => {
     const {favCountriesList} = useAppSelector((state) => state.favCountries)
+    const dispatch = useAppDispatch()
     return (
         <div id='fav-country'>
             <div>{favCountriesList.map(item => (
             <ul key={item.name.common}>
-                <li><img style={{ width: 20 }} src={item.flags.png} alt="flag" /><Link to= {`/bof-frontend-project-basic/${item.name.common}`}>{item.name.common}</Link></li>
+                <li><img style={{ width: 20 }} src={item.flags.png} alt="flag" />
+                    <Link to= {`/bof-frontend-project-basic/${item.name.common}`}>
+                        {item.name.common}
+                    </Link>
+                    <button id='delete-fav-btn' onClick={() => {dispatch(remove(item.name.common))}}>Delete</button>
+                </li>
             </ul>))}
             </div>
         </div>)
